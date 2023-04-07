09169f04b27a1d46188950a27e6cd3f7b970aa9e
Shubhammuthariya <shubhamsainbtp11@gmail.com>
2023-04-07 22:30:02 +0530

    searchbyname

--- Dom.js
+++ Dom.js
@@ -8,5 +8,9 @@ let temp2 = document.getElementById("main-header")
 console.log(document.getElementById("main-header"))
 temp2.style.borderBottom = 'solid 5px #000';
 temp.style.color = "grey"
-
+let c = document.getElementsByClassName("list-group-item")
+c[2].style.backgroundColor = "green"
+console.log(c[2])
+for(let i=0; i<c.length; i++){
+c[i].style.fontWeight = 'bold'}
 
