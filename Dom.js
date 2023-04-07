e714fd4b0558d2f33967b2c882ad9fe3df77fff8
Shubhammuthariya <shubhamsainbtp11@gmail.com>
2023-04-07 17:22:19 +0530

    adding dom

---
+++ Dom.js
@@ -0,0 +1,10 @@
+console.log(document.h1);
+document.title = 123
+document.h2= "ram"
+console.log(document.forms);
+
+let temp = document.getElementById("header-title")
+let temp2 = document.getElementById("main-header")
+console.log(document.getElementById("main-header"))
+temp2.style.borderBottom = 'solid 5px #000';
+temp.style.color = "grey"
