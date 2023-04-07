1c26c5934efd78ed3390a1befd55f83393dea41a
Shubhammuthariya <shubhamsainbtp11@gmail.com>
2023-04-07 23:02:23 +0530

    GetElementsbyTAgName

--- Dom.js
+++ Dom.js
@@ -1,16 +1,23 @@
-console.log(document.h1);
-document.title = 123
-document.h2= "ram"
-console.log(document.forms);
+// console.log(document.h1);
+// document.h2= "ram"
+// console.log(document.forms);
 
-let temp = document.getElementById("header-title")
-let temp2 = document.getElementById("main-header")
-console.log(document.getElementById("main-header"))
-temp2.style.borderBottom = 'solid 5px #000';
-temp.style.color = "grey"
-let c = document.getElementsByClassName("list-group-item")
-c[2].style.backgroundColor = "green"
-console.log(c[2])
-for(let i=0; i<c.length; i++){
-c[i].style.fontWeight = 'bold'}
+// let temp = document.getElementById("header-title")
+// let temp2 = document.getElementById("main-header")
+// console.log(document.getElementById("main-header"))
+// temp2.style.borderBottom = 'solid 5px #000';
+// temp.style.color = "grey"
+
+//getElementsByClassName
+// let c = document.getElementsByClassName("list-group-item")
+// c[3].style.backgroundColor = "green"
+// console.log(c)
+// for(let i=0; i<c.length; i++){
+// c[i].style.fontWeight = 'bold'}
+
+// GetElementsbyTAgName
+let li = document.getElementsByTagName("li")
+console.log(li)
+for(let i=0; i<li.length; i++){
+li[i].style.fontWeight = 'bold'}
 
