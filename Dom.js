d3a131e1a86287843d06f8f490920a2e56ba8b95
Shubhammuthariya <shubhamsainbtp11@gmail.com>
2023-04-08 23:56:33 +0530

    Creating Nodes and Modifying Dom

--- Dom.js
+++ Dom.js
@@ -23,8 +23,9 @@
 
 // queryselector 
 // let temp3 = document.querySelector(".list-group-item")
+// for(let i=0; i<temp3.clientHeight; i++){
 // temp3.style.color = "red"
-// console.log(temp3);
+// console.log(temp3);}
 
 // let temp4 = document.querySelector(".list-group-item:last-child")
 // temp4.style.olor = "teal"
@@ -38,14 +39,63 @@
 // temp6.style.display = "none"
 // console.log(temp6);
 
-// queryselectorall
-let find = document.querySelectorAll('.title')
-for(let i=0; i<find.length; i++){
-find[i].textContent = "red"}
-console.log(find)
 
-let odd = document.querySelectorAll('li:nth-child(odd)')
-for(let i = 0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'grey';
-}
+// queryselectorall
+// let find = document.querySelectorAll('.title')
+// for(let i=0; i<find.length; i++){
+// find[i].textContent = "red"}
+// console.log(find)
+
+// let odd = document.querySelectorAll('li:nth-child(odd)')
+// for(let i = 0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'grey';}
+// console.log(odd)
+
+// parentNode
+//let list = document.querySelector('#items')
+// console.log(list.parentNode);
+// console.log(list.parentNode.parentNode.parentNode)
+
+// //childNodes
+// console.log(list.childNodes);
+
+// // children
+// console.log(list.children);
+
+// first elementchild & lastelementchild
+// list.firstElementChild.style.backgroundColor = "red"
+// list.lastElementChild.style.backgroundColor = "red"
+// list.lastElementChild.textContent = 'hello 4'
+
+// SIBLING
+// console.log(list.nextSibling)
+// console.log(list.nextElementSibling)
+
+// console.log(list.previousSibling)
+// console.log(list.previousElementSibling)
+
+//create a Div
+let newDiv = document.createElement('div');
+
+// add class
+newDiv.className = 'hello'
+// add id
+newDiv.id = "text"
+// add attribute
+newDiv.setAttribute('title' , 'hello world')
+
+// create textNode 
+let text = document.createTextNode('hello world')
+// append textNode to div
+newDiv.appendChild(text);
+//console.log(newDiv)
+
+// push newDiv to Dom 
+// let tatto = document.querySelector('header .container')
+// let h1 = document.querySelector('header h1')
+// tatto.insertBefore(newDiv , h1)
+
+let tatto2 = document.querySelector('.list-group')
+let h2 = document.querySelector('.list-group-item')
+tatto2.insertBefore(newDiv , h2)
 
