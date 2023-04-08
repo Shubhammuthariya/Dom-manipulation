4ad054af3999b8ef86779ab67f93dc71f1670547
Shubhammuthariya <shubhamsainbtp11@gmail.com>
2023-04-08 11:45:39 +0530

    queryselector

--- Dom.js
+++ Dom.js
@@ -16,8 +16,36 @@
 // c[i].style.fontWeight = 'bold'}
 
 // GetElementsbyTAgName
-let li = document.getElementsByTagName("li")
-console.log(li)
-for(let i=0; i<li.length; i++){
-li[i].style.fontWeight = 'bold'}
+// let t = document.getElementsByTagName("li")
+// for(let i=0; i<t.length; i++){
+//     t[i].style.fontWeight = 'bold'
+// }
+
+// queryselector 
+// let temp3 = document.querySelector(".list-group-item")
+// temp3.style.color = "red"
+// console.log(temp3);
+
+// let temp4 = document.querySelector(".list-group-item:last-child")
+// temp4.style.olor = "teal"
+// console.log(temp4);
+
+// let temp5 = document.querySelector(".list-group-item:nth-child(2)")
+// temp5.style.backgroundColor = "green"
+// console.log(temp5);
+
+// let temp6 = document.querySelector(".list-group-item:nth-child(3)")
+// temp6.style.display = "none"
+// console.log(temp6);
+
+// queryselectorall
+let find = document.querySelectorAll('.title')
+for(let i=0; i<find.length; i++){
+find[i].textContent = "red"}
+console.log(find)
+
+let odd = document.querySelectorAll('li:nth-child(odd)')
+for(let i = 0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'grey';
+}
 
